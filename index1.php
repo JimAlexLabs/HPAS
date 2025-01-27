1b1f20bc26c995987b32099b8720ddd435b0196f
JimAlex <jb.jimalex@gmail.com>
2025-01-27 21:24:13 +0300

    third index1

--- index1.php
+++ index1.php
@@ -15,86 +15,76 @@ include("header.php");
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
 
     <link rel="stylesheet" type="text/css" href="style2.css">
-
   </head>
   <style type="text/css">
     #inputbtn:hover{cursor:pointer;}
     .card{
-    background: #f8f9fa;
-    border-top-left-radius: 5% 5%;
-    border-bottom-left-radius: 5% 5%;
-    border-top-right-radius: 5% 5%;
-    border-bottom-right-radius: 5% 5%;
-}
-
+      background: #f8f9fa;
+      border-top-left-radius: 5% 5%;
+      border-bottom-left-radius: 5% 5%;
+      border-top-right-radius: 5% 5%;
+      border-bottom-right-radius: 5% 5%;
+    }
   </style>
   <body style="background: -webkit-linear-gradient(left, #3931af, #00c6ff); background-size: cover;">
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav" >
-    <div class="container">
-
-      <a class="navbar-brand js-scroll-trigger" href="index.php" style="margin-top: 10px;margin-left:-65px;font-family: 'IBM Plex Sans', sans-serif;"><h4><i class="fa fa-user-plus" aria-hidden="true"></i>&nbsp THE ROYAL HOSPITAL PATIENT ADMINISTRATIVE SYSTEM</h4></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item" style="margin-right: 40px;">
-            <a class="nav-link js-scroll-trigger" href="index.php" style="color: white;font-family: 'IBM Plex Sans', sans-serif;"><h6>HOME</h6></a>
-          </li>
-  
-          <li class="nav-item" style="margin-right: 40px;">
-            <a class="nav-link js-scroll-trigger" href="services.html" style="color: white;font-family: 'IBM Plex Sans', sans-serif;"><h6>ABOUT US</h6></a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="contact.html" style="color: white;font-family: 'IBM Plex Sans', sans-serif;"><h6>CONTACT</h6></a>
-          </li>
-        </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="index.php" style="margin-top: 10px;margin-left:-65px;font-family: 'IBM Plex Sans', sans-serif;">
+          <h4><i class="fa fa-user-plus" aria-hidden="true"></i>&nbsp ROYAL HOSPITAL PATIENT ADMINISTRATIVE SYSTEM</h4>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item" style="margin-right: 40px;">
+              <a class="nav-link js-scroll-trigger" href="index.php" style="color: white;font-family: 'IBM Plex Sans', sans-serif;"><h6>HOME</h6></a>
+            </li>
+            <li class="nav-item" style="margin-right: 40px;">
+              <a class="nav-link js-scroll-trigger" href="services.html" style="color: white;font-family: 'IBM Plex Sans', sans-serif;"><h6>ABOUT US</h6></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="contact.html" style="color: white;font-family: 'IBM Plex Sans', sans-serif;"><h6>CONTACT</h6></a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-
-
+    </nav>
 
     <div class="container-fluid" style="margin-top:60px;margin-bottom:60px;color:#34495E;">
       <div class="row">
+        <div class="col-md-7" style="padding-left: 180px;">
+          <div style="color: white;">
+            <h4 style="font-family: 'IBM Plex Sans', sans-serif;">We are here for you!</h4>
+          </div>
+        </div>
 
-         <div class="col-md-7" style="padding-left: 180px; ">
-            <div style="color: white;">
-              <h4 style="font-family: 'IBM Plex Sans', sans-serif; margin-top: 150px;"> We are here for you!</h4>
-            </div>
-         </div>
-
-         <div class="col-md-4" style="margin-top: 5%;right: 8%">
+        <div class="col-md-4" style="margin-top: 5%;right: 8%">
           <div class="card" style="font-family: 'IBM Plex Sans', sans-serif;">
             <div class="card-body">
               <center>
                 <i class="fa fa-hospital-o fa-3x" aria-hidden="true" style="color:#0062cc"></i>
                 <br>
-              <h3 style="margin-top: 10%">Patient Login</h3><br>
-              <form class="form-group" method="POST" action="func.php">
-                <div class="row" style="margin-top: 10%">
-                  <div class="col-md-4"><label>Email-ID: </label></div>
-                  <div class="col-md-8"><input type="text" name="email" class="form-control" placeholder="enter email ID" required/></div><br><br>
-                  <div class="col-md-4" style="margin-top: 8%"><label>Password: </label></div>
-                  <div class="col-md-8" style="margin-top: 8%"><input type="password" class="form-control" name="password2" placeholder="enter password" required/></div><br><br><br>
-                </div>
-                <div class="row">
-                 <div class="col-md-4"  style="padding-left: 160px;margin-top: 10%">
-                    <center><input type="submit" id="inputbtn" name="patsub" value="Login" class="btn btn-primary"></center></div>           
-                </div>
-              </form>
-            </center>
+                <h3 style="margin-top: 10%">Patient Login</h3><br>
+                <form class="form-group" method="POST" action="func.php">
+                  <div class="row" style="margin-top: 10%">
+                    <div class="col-md-4"><label>Email-ID: </label></div>
+                    <div class="col-md-8"><input type="text" name="email" class="form-control" placeholder="enter email ID" required/></div><br><br>
+                    <div class="col-md-4" style="margin-top: 8%"><label>Password: </label></div>
+                    <div class="col-md-8" style="margin-top: 8%"><input type="password" class="form-control" name="password2" placeholder="enter password" required/></div><br><br><br>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-4" style="padding-left: 160px;margin-top: 10%">
+                      <center><input type="submit" id="inputbtn" name="patsub" value="Login" class="btn btn-primary"></center>
+                    </div>
+                  </div>
+                </form>
+              </center>
             </div>
           </div>
         </div>
-
-
       </div>
     </div>
-
-
-
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
