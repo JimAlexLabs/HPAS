3b053b8823dc0b014f1117296bd94a8f54900898
JimAlex <jb.jimalex@gmail.com>
2025-02-03 22:19:20 +0300

    mysql database

--- myhmsdb.sql
+++ myhmsdb.sql
@@ -12,7 +12,6 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -78,7 +77,9 @@ INSERT INTO `appointmenttb` (`pid`, `ID`, `fname`, `lname`, `gender`, `email`, `
 (4, 10, 'Mike', 'Rickter', 'Male', 'mikerickter6@gmail.com', '0723848946', 'Kay', 1000, '2025-03-25', '14:00:00', 1, 0),
 (4, 11, 'Mike', 'Rickter', 'Male', 'mikerickter7@gmail.com', '0723848946', 'Diana', 2000, '2025-03-27', '15:00:00', 1, 1),
 (9, 12, 'Dan', 'Kemutai', 'Male', 'dankemutai2@gmail.com', '0728683619', 'Koffi', 2500, '2025-03-26', '12:00:00', 1, 1),
-(9, 13, 'Dan', 'Kemutai', 'Male', 'dankemutai3@gmail.com', '0728683619', 'Tiana', 1800, '2025-03-26', '14:00:00', 1, 1); --------------------------------------------------------
+(9, 13, 'Dan', 'Kemutai', 'Male', 'dankemutai3@gmail.com', '0728683619', 'Tiana', 1800, '2025-03-26', '14:00:00', 1, 1);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `contact`
@@ -105,6 +106,7 @@ INSERT INTO `contact` (`name`, `email`, `contact`, `message`) VALUES
 ('Arman', 'arman@gmail.com', '0777889900', 'Your service is fantastic! I appreciate the quick response and assistance.'),
 ('Asiayi', 'asiayi@gmail.com', '0788990011', 'Thank you for your help! Your service is top-notch.'),
 ('Janet', 'janet@gmail.com', '0799001122', 'I love your service! It has made my life so much easier. Thank you!');
+
 -- --------------------------------------------------------
 
 --
@@ -132,6 +134,7 @@ INSERT INTO `doctb` (`username`, `password`, `email`, `spec`, `docFees`) VALUES
 ('Kay', 'kay123', 'kay@gmail.com', 'Cardiologist', 3000),
 ('Koffi', 'koffi123', 'koffi@gmail.com', 'Neurologist', 3500),
 ('Tiana', 'tiana123', 'tiana@gmail.com', 'Pediatrician', 1500);
+
 -- --------------------------------------------------------
 
 --
@@ -165,6 +168,7 @@ INSERT INTO `patreg` (`pid`, `fname`, `lname`, `gender`, `email`, `contact`, `pa
 (9, 'William', 'Ruto', 'Male', 'williamruto@gmail.com', '0799619153', 'william123', 'william123'),
 (10, 'Peter', 'Mwajuma', 'Male', 'petermwajuma@gmail.com', '0700362815', 'peter123', 'peter123'),
 (11, 'Sandra', 'Kajoohn', 'Female', 'sandrakajoohn@gmail.com', '0711768946', 'sandra123', 'sandra123');
+
 -- --------------------------------------------------------
 
 --
@@ -193,6 +197,7 @@ INSERT INTO `prestb` (`doctor`, `pid`, `ID`, `fname`, `lname`, `appdate`, `appti
 ('Kay', 2, 8, 'Angel', 'Rose', '2025-03-21', '10:00:00', 'Severe Fever', 'None', 'Take Paracetamol 500mg every 6 hours. Rest and drink plenty of fluids.'),
 ('Koffi', 9, 12, 'William', 'Ruto', '2025-03-26', '12:00:00', 'High Fever', 'None', 'Take Ibuprofen 400mg every 8 hours. Monitor temperature and rest.'),
 ('Tiana', 9, 13, 'William', 'Ruto', '2025-03-26', '14:00:00', 'Cough and Cold', 'Skin dryness', 'Take Loratadine 10mg daily. Drink warm water and consume fruits rich in vitamin C.');
+
 --
 -- Indexes for dumped tables
 --
